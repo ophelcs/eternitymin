28d41dc912fec05849a0a65798ad9961d4444c27
ophelcs <info@ophelconsultancyservices.com>
2026-01-09 13:46:41 -0600

    Add BottomTabs component for navigation

---
+++ components/BottomTabs.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BottomTabs: React.FC = () => {
+  return (
+    <nav style={{ padding: '1rem', borderTop: '1px solid #ddd' }}>
+      Home | Teachings | Training | Events | Give | More
+    </nav>
+  );
+};
+
+export default BottomTabs;
