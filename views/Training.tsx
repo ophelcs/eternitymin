5d32f24f76361189cc2bb06583c925c8bf844ee5
ophelcs <info@ophelconsultancyservices.com>
2026-01-09 14:08:06 -0600

    Rename HomeView to TrainingView with content update

--- views/Training.tsx
+++ views/Training.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
-export default function HomeView() {
-  return <div style={{ padding: "1rem" }}>Home</div>;
+export default function TrainingView() {
+  return (
+    <div style={{ padding: "16px" }}>
+      <h2 style={{ fontSize: "22px", fontWeight: 700 }}>Training</h2>
+      <p style={{ marginTop: "8px" }}>Courses, discipleship tracks, and resources will appear here.</p>
+    </div>
+  );
 }
