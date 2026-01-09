bee3ad0a2ea1531a6fb9bff7fd7efff8fb3a0246
ophelcs <info@ophelconsultancyservices.com>
2026-01-09 14:09:13 -0600

    Create MoreView component with content structure

--- views/More.tsx
+++ views/More.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
-export default function HomeView() {
-  return <div style={{ padding: "1rem" }}>Home</div>;
+export default function MoreView() {
+  return (
+    <div style={{ padding: "16px" }}>
+      <h2 style={{ fontSize: "22px", fontWeight: 700 }}>More</h2>
+      <p style={{ marginTop: "8px" }}>About, contact, prayer requests, and more links will be here.</p>
+    </div>
+  );
 }
