a38666820ea0306fdd9b20922b2d83abff71a7b5
ophelcs <info@ophelconsultancyservices.com>
2026-01-09 14:08:50 -0600

    Rename HomeView to GiveView with updated content

--- views/Give.tsx
+++ views/Give.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
-export default function HomeView() {
-  return <div style={{ padding: "1rem" }}>Home</div>;
+export default function GiveView() {
+  return (
+    <div style={{ padding: "16px" }}>
+      <h2 style={{ fontSize: "22px", fontWeight: 700 }}>Give</h2>
+      <p style={{ marginTop: "8px" }}>Online giving information will appear here.</p>
+    </div>
+  );
 }
