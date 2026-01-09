4d172231dc9c48c6cc23b5786a87c6372423f62d
ophelcs <info@ophelconsultancyservices.com>
2026-01-09 13:45:25 -0600

    Add Header component for Eternity Ministries

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header style={{ padding: '1rem', fontWeight: 'bold' }}>
+      Eternity Ministries
+    </header>
+  );
+};
+
+export default Header;
